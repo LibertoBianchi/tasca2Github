5bfb24c31bab4c9c7c851b3d36c1dba72ece16b4
Liberto <lbianchi@gmail.com>
2021-02-11 16:07:58 +0100

    Añadir contenido a clase Tauler

--- com/jaume/penjat/Tauler.java
+++ com/jaume/penjat/Tauler.java
@@ -1,4 +1,49 @@
 package com.jaume.penjat;
 
 public class Tauler {
+    public char[] getParaulaSecreta() {
+        return paraulaSecreta;
+    }
+
+    public void setParaulaSecreta(char[] paraulaSecreta) {
+        this.paraulaSecreta = paraulaSecreta;
+    }
+
+    private char[] paraulaSecreta;
+
+    public void inicialitzarPartida(String paraula, int i) {
+    }
+
+
+    public void setIntents() {
+    }
+
+
+    public String verificar(String ll) {
+    }
+
+    public Object[] getPalabraEndevinada() {
+        return palabraEndevinada;
+    }
+
+    public void setPalabraEndevinada(Object[] palabraEndevinada) {
+        this.palabraEndevinada = palabraEndevinada;
+    }
+
+    public String imprimir() {
+    }
+
+    public String imprimirVides() {
+    }
+
+    public boolean hasGuanyat() {
+        return false;
+    }
+
+    public void setIntents() {
+    }
+
+    public String getIntents() {
+    }
+
 }
